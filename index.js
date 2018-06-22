acec805e3f0d89c8138d9438b2e00420a310f8cf
wcrbrm <viclopata@gmail.com>
2018-06-22 20:40:47 +0300

    index

---
+++ index.js
@@ -0,0 +1,16 @@
+/*
+
+API of the interface:
+  POST /:network/create
+  GET /:network/import/options
+  POST /:network/import
+  POST /wallets
+  GET /wallets - get current wallets
+  POST /exchanges
+  GET /wallets
+  GET /settings
+  PUT /settings
+  GET /:network/vote
+  POST /:network/vote
+
+*/
