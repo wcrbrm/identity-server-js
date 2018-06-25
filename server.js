14bfaeed02485514df7fab29a4bc674e1a854cc5
wcrbrm <viclopata@gmail.com>
2018-06-25 12:35:56 +0300

    command line options added

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 const http = require("http");
 const express = require("express");
+const program = require('commander');
 const bodyParser = require('body-parser');
-const cors = require('cors');
 
-const port = process.env.PORT || 7773;
+program
+    .option('--cors', 'Enable CORS', false)
+    .option('-h, --host [host]', 'Bind to address (127.0.0.1 by default)')
+    .option('-p, --port [port]', 'Port to be launched (7773  by default)')
+    .option('-v, --verbose', 'Increase verbosity', true, 40)
+    .parse(process.argv);
+
+console.log(program.host, program.port, program.cors);
+process.exit(0);
+
+const host = program.host || "127.0.0.1";
+const port = program.port || 7773;
+
 const app = express();
 app.use(bodyParser.json());
-app.use(cors());
+
+if (program.cors) {
+  app.use(require('cors')());
+}
 
 app.get('/api/status', require('./api/status'));
 
@@ -32,7 +47,7 @@ app.get('/', (req, res, next) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-console.log("app listening on %d ", port);
-app.listen(port, "127.0.0.1");
+console.log("app listening on %s:%d ", host, port);
+app.listen(port, host);
 
 
