053853da2e087440aeb75fc78a5de5d96a797b6b
wcrbrm <viclopata@gmail.com>
2018-07-10 11:31:45 +0300

    notes on server api

--- server.js
+++ server.js
@@ -9,25 +9,74 @@ app.use(bodyParser.json());
 if (options.cors) {
   app.use(require('cors')());
 }
+const root = '/api';
 
-app.get('/api/status', require('./api/status')(options));
+app.get(`${root}/status`, require('./api/status')(options));
 
 /*
+API of the installation
 
-API of the interface:
-  POST /:network/create
-  GET /:network/import/options
-  POST /:network/import
-  POST /wallets
-  GET /wallets - get current wallets
-  POST /exchanges
-  GET /wallets
-  GET /settings
-  PUT /settings
-  GET /:network/vote
-  POST /:network/vote
+// status should provide public key of the setup
+
+// and the endpoint for creating storage, should go encrypted
+// for decoding only with the private key
+app.post(`${root}/storage`, require('./api/install/storage'));
+
+// endpoint that is used for restoring
+// should accept publicKey
+app.post(`${root}/restore`, require('./api/install/restore'));
+
+// endpoint to create/update pairing password
+app.post(`${root}/pair`, require('./api/install/pair')();
+// endpoint to establist a connection using pairing password
+app.post(`${root}/connect`, require('./api/install/connect')());
+
+app.get(`${root}/settings`, require('./api/settings')());
+app.post(`${root}/settings`, require('./api/settings')());
+
+// do we need lock and unlock endpoints at all?
+// at first sight it looks insecure
+// may be it should be just always locked
+app.post(`${root}/lock`, require('./api/lock')());
+app.post(`${root}/unlock`, require('./api/unlock`)());
+
+// getting list of the wallets
+app.get(`${root}/wallets`, require('./api/wallets/list')());
+
+app.post(`${root}/wallets/create`, require('./api/wallets/create')());
+app.post(`${root}/wallets/import`, require('./api/wallets/import')());
+
+
+// pairing could actually be useful only to remote connections
+// remote access tab can be a part of installation wizard or a part of the settings?
+
+// pair validation is basically (remote IP address + browser)??
+// cannot be for keeping remote installatino
 
 */
+
+// MAIN QUESTION: how to pair?
+// proposal 1. 
+
+
+// binding, on successfull installation it returns
+// a pair of public/secret key (generated for the storage)
+// it could be small enough?
+
+// Those keys should be written down (copied)
+// [X] I want this storage to be accessible remotely
+
+// In case of pairing,
+// that secret key should be stored in localStorage
+
+// secret cannot be used directly!
+// we can use secret to put a signed message for every request
+
+// Multiple storages? Multiple pairing keys + addresses
+// masterwallet_pairing variable is a JSON
+// Should the same pairing be in the plugin? who knows..
+
+// PAIR = (PUBLIC, SECRET, URL)
 
 // Dummy page for the api
 app.get('/', (req, res, next) => {
