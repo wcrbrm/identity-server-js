12a940f3ba49fea7c604af89b301dbf14b2058e2
wcrbrm <viclopata@gmail.com>
2018-08-09 10:31:02 +0300

    EIP55 code cleanup

--- services/eip55.js
+++ services/eip55.js
@@ -1,17 +1,17 @@
 const ethUtil = require('ethereumjs-util');
 
-const toChecksumAddress = function (address) {
-  address = ethUtil.stripHexPrefix(address).toLowerCase()
-  var hash = ethUtil.sha3(address).toString('hex')
-  var ret = '0x';
-  for (var i = 0; i < address.length; i++) {
+const toChecksumAddress = (address) => {
+  address = ethUtil.stripHexPrefix(address).toLowerCase();
+  const hash = ethUtil.sha3(address).toString('hex');
+  let ret = '0x';
+  for (let i = 0; i < address.length; i++) {
     if (parseInt(hash[i], 16) >= 8) {
-      ret += address[i].toUpperCase()
+      ret += address[i].toUpperCase();
     } else {
-      ret += address[i]
+      ret += address[i];
     }
   }
-  return ret
+  return ret;
 };
 
 module.exports = { toChecksumAddress };
