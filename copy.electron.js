743e432c204e6e12861fe5165136d98d4857e3c9
wcrbrm <viclopata@gmail.com>
2018-09-14 15:35:41 +0300

    copy script: linking support

--- copy.electron.js
+++ copy.electron.js
@@ -1,28 +1,54 @@
 const fs = require('fs')
+const path = require('path')
 const shell = require('shelljs')
 const excluded = [ ".git", ".gitignore", "README.md", "LICENSE", "copy.electron.js", "package-lock.json" ];
+const program = require("commander");
 
-excluded.push("node_modules")
+program
+ .option("--skip-node-modules", "Skip node_modules")
+ .option("--link", "Link instead of copying")
+ .parse(process.argv);
+
+if (program.skipNodeModules) {
+   excluded.push("node_modules")
+}
+
+const linking = !!program.link;
+const desktop_dir = path.dirname(__dirname) + "/masterwallet-desktop";
+
+console.log("destination: ", desktop_dir);
 
 fs.readdir(".", (err, items) => {
   for (let i = 0; i < items.length; i++) {
      const file = items[i];
      if (excluded.indexOf(file) !== -1) continue;
-     console.log("copying " + file);
-     shell.exec("cp -rf " + file + " ../masterwallet-desktop/");
+     const allowLink = file.indexOf(".json") === -1;
+    
+     console.log((linking && allowLink ? "linking " : "copying ") + file);
+     shell.exec((linking && allowLink) ? 
+         "ln -s " + __dirname + "/" + file + " " + desktop_dir + "/" +  file
+       : "cp -rf " + __dirname + "/" + file + " " + desktop_dir + "/");
   }
-  shell.exec("rm -f ../masterwallet-desktop/network/*/*.test.js");
-  shell.exec("rm -f ../masterwallet-desktop/ipc/*.test.js");
-  shell.exec("rm -f ../masterwallet-desktop/api/*.test.js");
+  if (!linking) {
+    console.log("removing tests");
+    shell.exec("rm -f " + desktop_dir + "/network/*/*.test.js");
+    shell.exec("rm -f " + desktop_dir + "/ipc/*.test.js");
+    shell.exec("rm -f " + desktop_dir + "/api/*.test.js");
+  }
 
-  const fnJson = "./../masterwallet-desktop/package.json";
+  console.log("updating package.json");
+  const fnJson = desktop_dir + "/package.json";
   const json = JSON.parse(fs.readFileSync(fnJson));
 
   json.name = "masterwallet-desktop";
   json.description = "MasterWallet Pro - Desktop Wallet for CryptoAsset Management";
   json.main = "main.js";
+  json.scripts = { 
+     "app": "cross-env DEBUG=* electron main.js" 
+  };
 
   fs.writeFileSync(fnJson, JSON.stringify(json, null, 2));
 });
+
 
 
